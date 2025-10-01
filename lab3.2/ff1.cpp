813a719a8a7676f24a357c5b3ab3692b608a021f
arturyakubovychri2025-afk <artur.yakubovych.ri.2025@lpnu.ua>
2025-10-01 12:20:16 +0300

    4.3

--- lab3.2/ff1.cpp
+++ lab3.2/ff1.cpp
@@ -1,41 +1,97 @@
-// lab_03_02.cpp
+// lab_04_03.cpp
 // Якубович Артур
 // Лабораторна робота № 3.2
 // Розгалуження, задане формулою: функція з параметрами
 // Варіант 28
 
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+
 using namespace std;
+// a=2  c=-3  xp=-2  xk=2  dx=1
+// a=0  c=5  xp=1  xk=3  dx=1
+
 int main() {
-	double x;
-	double a;
-	double c;
-	double F;
+    double x;
+    double a;
+    double c;
+    double F;
+    double xp, xk, dx;
 
-	cout << "a = "; cin >> a;
-	cout << "c = "; cin >> c;
-	cout << "x = "; cin >> x;
+    cout << "a = "; cin >> a;
+    cout << "c = "; cin >> c;
+    cout << "x = "; cin >> x;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << fixed;
+    cout << "-----------------------" << endl;
+    cout << "|" << setw(10) << "x" << "|" << setw(10) << "F" << "|" << endl;
+    cout << "-----------------------" << endl;
+    x = xp;
+    while (x <= xk) {
+        if ((c < 0) && (a != 0))
+            F = -a * pow(x, 2);
+        if ((c > 0) && (a == 0))
+            F = (a - x) / (c * x);
+        if (!(c < 0 && a != 0) && !(c < 0 && a == 0))
+            F = x / c;
+        cout << "|" << setw(10) << setprecision(2) << x << "|" << setw(10) << setprecision(2) << F << "|" << endl;
+        x += dx;
+    }
+    cout << "-----------------------" << endl;
+    F = 0;
+    x = xp;
+    cout << endl;
+    cout << "-----------------------" << endl;
+    cout << "|" << setw(10) << "x" << "|" << setw(10) << "F" << "|" << endl;
+    cout << "-----------------------" << endl;
 
-	// спосіб 1: розгалуження в скороченій формі
-	if ((a < 0) && (x != 0))
-		F = -a * pow(x, 2);
-	if ((c > 0) && (a == 0))
-		F = (a - x) / c * x;
-	else
-		F = 1 + (x / c);
-	cout << endl;
-	cout << "1) F = " << F << endl;
 
-	// спосіб 2: рогалуження в повній формі
-	if (c < 0 && a != 0)
-		F = -a * pow(x, 2);
-	else
-		if (c > 0 && a == 0)
-			F = (a - x) / c * x;
-		else
-			F = 1 + (x / c);
-	cout << "F = " << F << endl;
+  do {
+        if ((c < 0) && (a != 0))
+            F = -a * pow(x, 2);
+        if ((c > 0) && (a == 0))
+            F = (a - x) / (c * x);
+        if (!(c < 0 && a != 0) && !(c < 0 && a == 0))
+            F = x / c;
+        cout << "|" << setw(10) << setprecision(2) << x << "|" << setw(10) << setprecision(2) << F << "|" << endl;
+        x += dx;
+    } while (x <= xk);
+    cout << "-----------------------" << endl;
+    cout << endl;
+    cout << "-----------------------" << endl;
+    cout << "|" << setw(10) << "x" << "|" << setw(10) << "F" << "|" << endl;
+    cout << "-----------------------" << endl;
+    F = 0;
+    x = xp;
 
-	cin.get();
-	return 0;
+    for (x; x <= xk; x += dx) {
+        if ((c < 0) && (a != 0))
+            F = -a * pow(x, 2);
+        if ((c > 0) && (a == 0))
+            F = (a - x) / (c * x);
+        if (!(c < 0 && a != 0) && !(c < 0 && a == 0))
+            F = x / c;
+        cout << "|" << setw(10) << setprecision(2) << x << "|" << setw(10) << setprecision(2) << F << "|" << endl;
+    }
+    cout << "-----------------------" << endl;
+
+    cout << endl;
+    cout << "-----------------------" << endl;
+    cout << "|" << setw(10) << "x" << "|" << setw(10) << "F" << "|" << endl;
+    cout << "-----------------------" << endl;
+    for (x = xk; x >= xp; x -= dx) {
+        if ((c < 0) && (a != 0))
+            F = -a * pow(x, 2);
+        if ((c > 0) && (a == 0))
+            F = (a - x) / (c * x);
+        if (!(c < 0 && a != 0) && !(c < 0 && a == 0))
+            F = x / c;
+        cout << "|" << setw(10) << setprecision(2) << x << "|" << setw(10) << setprecision(2) << F << "|" << endl;
+    }
+    cout << "-----------------------" << endl;
+    cin.get();
+    return 0;
 }
